7055c9f77b99e59084736c6cc7fe5a25631afc34
Juan David Mercado Lopez <juandmercadolopez@gmail.com>
2021-06-13 15:08:28 -0500

    estructura base

---
+++ src/main/java/io/github/juandmercadolopez/mercadolibre/challenge/api/MutantsApplication.java
@@ -0,0 +1,13 @@
+package io.github.juandmercadolopez.mercadolibre.challenge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/juandmercadolopez/mercadolibre/challenge/api/MutantsChallengeMercadolibreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.juandmercadolopez.mercadolibre.challenge.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantsChallengeMercadolibreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
